8c8af93f8d69fec11832e18e7f1261e4248f57c9
Nicola Mustone <mustone.nicola@gmail.com>
2015-09-16 17:34:24 +0200

    updated config sample file

--- tests/config.sample.php
+++ tests/config.sample.php
@@ -3,3 +3,12 @@ global $test_config;
 
 $test_config['token']       = 'xxxxxxxxxxxxx';
 $test_config['admin-email'] = 'name@domain.com';
+
+/**
+ * Stop editing here
+ */
+
+$test_config['dummy-data']  = [
+    'video' => __DIR__ . '/dummy-data/dummy-video.mp4',
+    'image' => __DIR__ . '/dummy-data/dummy-image.jpg',
+];
