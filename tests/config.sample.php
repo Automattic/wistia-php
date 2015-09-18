ddee4f2aae347383474d7e7e8f76fd6c00a5221e
Nicola Mustone <mustone.nicola@gmail.com>
2015-09-18 17:13:59 +0200

    updated config sample

--- tests/config.sample.php
+++ tests/config.sample.php
@@ -8,7 +8,8 @@ $test_config['admin-email'] = 'name@domain.com';
  * Stop editing here
  */
 
-$test_config['dummy-data']  = [
-    'video' => __DIR__ . '/dummy-data/dummy-video.mp4',
-    'image' => __DIR__ . '/dummy-data/dummy-image.jpg',
-];
+ $test_config['dummy-data']  = [
+     'video'    => __DIR__ . '/dummy-data/dummy-video.mov',
+     'image'    => __DIR__ . '/dummy-data/dummy-image.jpg',
+     'captions' => __DIR__ . '/dummy-data/dummy-captions.srt',
+ ];
